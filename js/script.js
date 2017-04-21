cb921f85c0e53186ee345e62379635c94fc1a3df
Gurbhej Singh <gurbhejgill16@gmail.com>
2017-04-21 18:17:57 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(window).ready(function(){
+        $("#DIVMenu").animate({left: '250px'}, 1500);
+    });
+
+$(window).ready(function(){
+       $(".sb_title").animate({top: '0px'}, 1500);
+    });
+
+$(window).ready(function(){
+      $("#sb_rt_subwrap").animate({right: '250px'}, 1500);
+    });
